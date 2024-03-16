e6ff9ca524edf7a717395cc1a03cc0649a649a99
Anurag Raushan <anuragraushan373@gmail.com>
2024-03-16 16:55:12 +0530

    bug fixed

---
+++ src/app/create/Main.jsx
@@ -0,0 +1,51 @@
+"use client";
+import FormComponent from "@/components/FormComponent";
+import React, { useState } from "react";
+
+const Main = () => {
+  const [head, setHead] = useState("Form Title");
+  const [modelOpen, setModelOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModelOpen(!modelOpen);
+  };
+
+  const headChange = () => {
+    return (e) => {
+      setHead(e.target.value);
+      if (e.target.value === "") setHead("Form Title");
+    };
+  };
+  return (
+    <>
+      <div className="create-top">
+        <div className="create-inner-top">
+          <div className="create-head">
+            {">>"} {head === "Form Title" ? "Untitled" : head}
+          </div>
+          <div className="create-content">
+            <div className="input-head">
+              <input type="text" placeholder={head} onChange={headChange()} />
+              <hr />
+
+              <div className="comp-data">
+                <FormComponent />
+              </div>
+
+              <div className="create-add" onClick={toggleModal}>
+                Add New Component
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {modelOpen && (
+        <div className="create-modal">
+          <Modal toggle={toggleModal} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Main;
--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,52 +1,7 @@
-"use client";
-import FormComponent from "@/components/FormComponent";
-import Modal from "@/components/modal";
-import React, { useState } from "react";
+import Main from "./Main";
 
 function Page() {
-  const [head, setHead] = useState("Form Title");
-  const [modelOpen, setModelOpen] = useState(false);
-
-  const toggleModal = () => {
-    setModelOpen(!modelOpen);
-  };
-
-  const headChange = () => {
-    return (e) => {
-      setHead(e.target.value);
-      if (e.target.value === "") setHead("Form Title");
-    };
-  };
-  return (
-    <>
-      <div className="create-top">
-        <div className="create-inner-top">
-          <div className="create-head">
-            {">>"} {head === "Form Title" ? "Untitled" : head}
-          </div>
-          <div className="create-content">
-            <div className="input-head">
-              <input type="text" placeholder={head} onChange={headChange()} />
-              <hr />
-
-              <div className="comp-data">
-                <FormComponent />
-              </div>
-
-              <div className="create-add" onClick={toggleModal}>
-                Add New Component
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {modelOpen && (
-        <div className="create-modal">
-          <Modal toggle={toggleModal} />
-        </div>
-      )}
-    </>
-  );
+  return <Main />;
 }
 
 export default Page;
